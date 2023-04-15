b918f1cc05a68942b4a51b8fc86052265ffb1b96
cccortesc1 <cc.cortesc1@uniandes.edu.co>
2023-04-15 00:47:12 -0500

    Ajuste en el despliegue compose

--- converter/__init__.py
+++ converter/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///converter.db'
+    db_url = "postgresql://postgres:postgres@localhost:5432/cloud_conversion"
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url #'sqlite:///converter.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['JWT_SECRET_KEY'] = 'platipus'
